f412852b189ea11742b6e81b78016bf8b7eb732e
Roger Filmyer <roger.filmyer@gmail.com>
2014-04-13 00:50:37 -0400

    First submission.

---
+++ gcng.py
@@ -0,0 +1,26 @@
+"""things to do
+open files
+convert into lists
+pick randomly"""
+
+import random
+
+def OpenAsCleanList(filename):
+    '''Opens a file and converts into a list.
+    Ignores comments preceded with "#" and blank lines
+    
+    >>>OpenAsCleanList("motd.txt")
+    ["the first rule","is that you do not talk about fight club"]
+    '''
+    list = []
+    with open(filename) as f:
+        for line in f:
+            li = line.rstrip()
+            if not li.startswith("#") and li:
+                list.append(li)
+        return list
+
+first = OpenAsCleanList("first.txt")
+second = OpenAsCleanList("second.txt")
+
+print(random.choice(first) + ' ' + random.choice(second))
