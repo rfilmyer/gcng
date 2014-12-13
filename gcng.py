f3a5a33b3c6556517ce66e34f8484cde26480756
Roger Filmyer <roger.filmyer@gmail.com>
2014-12-13 09:41:38 -0500

    Function name now PEP8 friendly

--- gcng.py
+++ gcng.py
@@ -1,8 +1,8 @@
-def OpenAsCleanList(filename):
+def open_as_clean_list(filename):
     '''Opens a file and converts into a list.
     Ignores comments preceded with "#" and blank lines
     
-    >>>OpenAsCleanList("motd.txt")
+    >>>open_as_clean_list("motd.txt")
     ["the first rule","is that you do not talk about fight club"]
     '''
     list = []
@@ -16,6 +16,6 @@ def OpenAsCleanList(filename):
 if __name__ == '__main__':
     import random
     
-    first = OpenAsCleanList("first.txt")
-    second = OpenAsCleanList("second.txt")
+    first = open_as_clean_list("first.txt")
+    second = open_as_clean_list("second.txt")
     print(random.choice(first) + ' ' + random.choice(second))
